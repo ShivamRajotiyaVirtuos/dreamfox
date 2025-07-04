fcb177972b4033e391b5d96493618dd90593dad9
Shivam Rajotiya <ShivamRajotiyaVirtuos.com>
2025-07-04 10:13:36 +0530

    solved build error

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,13 @@
 import Banner from "@/components/Homepage/banner";
 import OurVision from "@/components/Homepage/ourvision";
 import Snapshot from "@/components/Homepage/snapshot";
-import Vision from "@/components/Homepage/vision";
 import WhatWeDo from "@/components/Homepage/what_we_do";
 
 export default function Home() {
   return (
     <div className="bg-[#1a1a1a] min-h-screen">
-      {/* <Navbar /> */}
       <Banner />
       <WhatWeDo />
-      {/* <Vision /> */}
       <OurVision />
       <Snapshot />
     </div>
