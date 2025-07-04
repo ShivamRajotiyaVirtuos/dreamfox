dacb4d67f7354350386b3454ee0d9cdbdc2c9031
Siddhant Sharma <siddhant.sharma@virtuos.com>
2025-07-04 10:35:28 +0530

    refactor: enable dynamic import for WebGL component to prevent SSR issues. To fix build error

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ['gsap'],
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+    }
+    return config
+  }
 };
 
 export default nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import Layout from '@/components/Layout/layout'
-import WebGLCursorEffect from '@/components/Webgl/webgl'
 import '@/styles/globals.css'
+import dynamic from 'next/dynamic'
 
+// Dynamically import WebGL component to avoid SSR issues
+const WebGLCursorEffect = dynamic(
+  () => import('@/components/Webgl/webgl'),
+  { ssr: false }
+)
 
 export default function App({ Component, pageProps }) {
   return (
