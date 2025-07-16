a49b1df069ae23f32eb52ca29fa0d5ecf7f03a8e
Shivam Rajotiya <ShivamRajotiyaVirtuos.com>
2025-07-16 17:51:15 +0530

    services section

--- pages/services/brand-advisory/index.jsx
+++ pages/services/brand-advisory/index.jsx
@@ -1,10 +1,13 @@
 import BrandAdvisoryProcess from "@/components/Services/Brand-Advisory/process";
+import Services_Brand from "@/components/Services/Brand-Advisory/services";
 import React from "react";
 
 const Brand = () => {
   return (
     <div>
+      <Services_Brand/>
       <BrandAdvisoryProcess />
+      
     </div>
   );
 };
