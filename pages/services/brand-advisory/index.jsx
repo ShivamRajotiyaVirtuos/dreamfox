8a35967b5d7e74287bc6afa7edc3ed17eb445b88
Siddhant Sharma <siddhant.sharma@virtuos.com>
2025-07-21 15:01:43 +0530

    Add ProcessTimeline component for enhanced brand advisory experience

--- pages/services/brand-advisory/index.jsx
+++ pages/services/brand-advisory/index.jsx
@@ -2,6 +2,7 @@ import Banner_Brand from "@/components/BrandAdvisory/Banner";
 import BeforeAfterGallery from "@/components/BrandAdvisory/BeforeAfterGallery";
 import OurWork from "@/components/BrandAdvisory/OurWork";
 import BrandAdvisoryProcess from "@/components/BrandAdvisory/process";
+import ProcessTimeline from "@/components/BrandAdvisory/ProcessTimeline";
 import Services from "@/components/BrandAdvisory/services";
 import React from "react";
 
@@ -9,6 +10,7 @@ const Brand = () => {
   return (
     <div>
       <Banner_Brand />
+      <ProcessTimeline />
       <Services />
       <OurWork />
       <BeforeAfterGallery />
