42c56544a88228cf07f4ab21c0983219e219a923
Shivam Rajotiya <ShivamRajotiyaVirtuos.com>
2025-07-24 17:56:11 +0530

    feat: add Experience Mix banner and horizontal scroll gallery; update Navbar links and styles

--- components/Navbar/Navbar2.jsx
+++ components/Navbar/Navbar2.jsx
@@ -164,10 +164,10 @@ const Navbar = () => {
                     UI/UX DESIGN
                   </a>
                   <a
-                    href="#"
-                    className="block px-6 py-3 text-white font-medium text-base hover:text-[#ec466f] hover:bg-white/10 transition-all duration-300 ease-out rounded-xl transform hover:translate-x-2"
+                    href="/services/experience-mix"
+                    className="block uppercase px-6 py-3 text-white font-medium text-base hover:text-[#ec466f] hover:bg-white/10 transition-all duration-300 ease-out rounded-xl transform hover:translate-x-2"
                   >
-                    WEB DEVELOPMENT
+                    Experience Mix
                   </a>
                 </div>
               </div>
@@ -226,7 +226,7 @@ const Navbar = () => {
               onMouseEnter={handleMouseEnter}
               onMouseMove={handleMouseMove}
               onMouseLeave={handleMouseLeave}
-              className="relative px-8 cursor-pointer py-4  font-black text-lg border-[#e83682] border-2 rounded-full overflow-hidden group hover:shadow-2xl hover:shadow-purple-500/25 transition-all duration-300 hover:scale-105 flex items-center gap-3"
+              className="relative px-8 cursor-pointer py-4  font-black text-lg border-[#fff] border-2 rounded-full overflow-hidden group hover:shadow-2xl hover:shadow-purple-500/25 transition-all duration-300 hover:scale-105 flex items-center gap-3"
             >
               {/* Overlay effect */}
               <div
@@ -240,7 +240,7 @@ const Navbar = () => {
                 }}
               />
 
-              <span className="relative z-10 text-[#e83682] group-hover:text-white ">CONTACT US</span>
+              <span className="relative z-10 text-white ">CONTACT US</span>
 
               <span className=" rounded-full relative z-10">
                 <svg
