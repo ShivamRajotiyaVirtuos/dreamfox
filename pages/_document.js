b5c2ee878d5731b258cc3ae080946cc3b1df76a1
Shivam Rajotiya <shivam.rajotiya@virtuos.com>
2025-08-26 12:43:34 +0530

    added favicon

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,18 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Favicon links */}
+        <link rel="icon" href="/logos/dreamfox_emblem.svg" type="image/svg+xml" />
+        <link rel="alternate icon" href="/favicon.ico" />
+        <link rel="mask-icon" href="/logos/dreamfox_emblem.svg" color="#E62D8D" />
+
+        {/* Apple Touch Icon */}
+        <link rel="apple-touch-icon" href="/logos/dreamfox_emblem.svg" />
+
+        {/* Theme color for mobile browsers */}
+        <meta name="theme-color" content="#E62D8D" />
+      </Head>
       <meta name="robots" content="noindex, nofollow" />
       <meta
         name="viewport"
